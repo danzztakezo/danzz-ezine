3e103adf667b47a68a7845cd7772465eb3a3c438
danzz <danzztakezo@gmail.com>
2012-01-30 22:27:53 +0700

    backend core dan htaccess

--- application/modules/backend/controllers/backend.php
+++ application/modules/backend/controllers/backend.php
@@ -1,6 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Backend extends CI_Controller {
+class Backend extends MY_Controller {
+	function __construct()
+    {
+        parent::__construct();
+    }
 
   public function index(){
     $data = array();
