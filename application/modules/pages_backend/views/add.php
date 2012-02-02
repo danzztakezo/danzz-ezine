31c34c9ce92157047c36dcfed74429ca5d70f0ae
dani <dani.himawan@kiranatama.com>
2012-02-03 01:28:17 +0700

    from add profile

---
+++ application/modules/pages_backend/views/add.php
@@ -0,0 +1,11 @@
+<div id="add_form" class="form">
+  <h2> Tambah Profile </h2>
+    <div id="contentLeft">
+      <form method="post" action="#">
+       <span ><label for="name"> Nama :</label> <input type="text" name="name" id="name"/> </span>
+       <span ><label for="intitle"> Title :</label> <input type="text" name="intitle" id="intitle"/> </span>
+       <span ><label for="incontent"> Deskripsi :</label> <textarea name="incontent" id="incontent" cols="5" rows="5"> </textarea> </span>
+       <input type="button" name="btnsimpan" id="btnsimpan" value="Simpan"/> 
+      </form>
+    </div>
+</div>
